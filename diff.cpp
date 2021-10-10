8d152553435df9c7ab28cce78b9b336edd5d9fb3
justmeandcp <91773245+justmeandcp@users.noreply.github.com>
2021-10-11 00:06:23 +0530

    Create diff.cpp
    Uses vector which increases space comp

---
+++ diff.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void inorder(TreeNode* root,vector<int> &v)
+    {
+        if(root==NULL)
+        return;
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
+    }
+    int minDiffInBST(TreeNode* root) {
+        vector<int> v;
+        inorder(root,v);
+        int mini=INT_MAX;
+        for(int i=1;i<v.size();i++)
+        {
+            mini=min(mini,v[i]-v[i-1]);
+        }
+        return mini;        
+    }
+};
